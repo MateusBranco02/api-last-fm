85ce4ad42995ed7ad12899e9bfb2d34322bf48e0
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2024-12-10 14:31:41 -0300

    Finalizado

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,15 +7,20 @@ export default function Home() {
     let apiKey = 'b19cdb5c459071ba668d0ef3d15e4e9d';
     const [nomeDoAlbum, setNomeDoAlbum] = useState('');
     const [albuns, setAlbuns] = useState([]);
+    const [carregando, setCarregando] = useState(false);
 
     const buscarAlbuns = async () => {
+        setCarregando(true);
         try {
             const url = `https://ws.audioscrobbler.com/2.0/?method=album.search&album=${nomeDoAlbum}&api_key=${apiKey}&format=json`;
             const response = await axios.get(url);
             console.log(response.data.results.albummatches.album);
             setAlbuns(response.data.results.albummatches.album);
+            setNomeDoAlbum('');
         } catch (error) {
             console.error(error);
+        } finally {
+            setCarregando(false);
         }
     }
 
@@ -34,23 +39,33 @@ export default function Home() {
                 </div>
 
                 <div className='container-albuns'>
-                    <div className='albuns'>
-                        <ul className='lista-albuns'>
-                            {albuns.map((album, key) => (
-                                <Link to={`/musicas/${album.artist}/${album.name}`} key={key}>
-                                    <li className='item-album'>
-                                        <div className='container-img'>
-                                            <img className='img' src={album.image[2]?.['#text']} alt={album.name} />
-                                        </div>
-                                        <div className='container-infos'>
-                                            <p className='p-nome'>{album.name}</p>
-                                            <p className='p-artista'>{album.artist}</p>
-                                        </div>
-                                    </li>
-                                </Link>
-                            ))}
-                        </ul>
-                    </div>
+                    {carregando ? (
+                        <>
+                            <div className='container-carregando'>
+                                <h2>Carregando...</h2>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className='albuns'>
+                                <ul className='lista-albuns'>
+                                    {albuns.map((album, key) => (
+                                        <Link to={`/musicas/${album.artist}/${album.name}`} key={key}>
+                                            <li className='item-album'>
+                                                <div className='container-img'>
+                                                    <img className='img' src={album.image[2]?.['#text']} alt={album.name} />
+                                                </div>
+                                                <div className='container-infos'>
+                                                    <p className='p-nome'>{album.name}</p>
+                                                    <p className='p-artista'>{album.artist}</p>
+                                                </div>
+                                            </li>
+                                        </Link>
+                                    ))}
+                                </ul>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </>
--- src/Musicas.jsx
+++ src/Musicas.jsx
@@ -7,19 +7,27 @@ export default function Musicas() {
     let apiKey = 'b19cdb5c459071ba668d0ef3d15e4e9d';
     const { artista, nomeDoAlbum } = useParams();
     const [musicas, setMusicas] = useState([]);
+    const [informacoesDoAlbum, setInformacoesDoAlbum] = useState({});
+    const [tags, setTags] = useState([]);
+    const [carregando, setCarregando] = useState(false);
 
     useEffect(() => {
         musicasDoArtista();
     }, [])
 
     const musicasDoArtista = async () => {
+        setCarregando(true);
         try {
             const url = `https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${apiKey}&artist=${artista}&album=${nomeDoAlbum}&format=json`;
             const response = await axios.get(url);
             console.log(response.data.album.tracks.track);
-            setMusicas(response.data.album);
+            setMusicas(response.data.album.tracks.track || []);
+            setInformacoesDoAlbum(response.data.album);
+            setTags(response.data.album.tags.tag || []);
         } catch (error) {
             console.error(error);
+        } finally {
+            setCarregando(false);
         }
     }
 
@@ -31,13 +39,47 @@ export default function Musicas() {
                 </div>
 
                 <div className='container-musicas'>
-                    {musicas.image ? (
+                    {carregando ? (
                         <>
-                            {console.log(musicas)}
-                            <img src={musicas?.image[2]['#text']} alt={musicas.name} />
-                            <p>Nome: {musicas?.name}</p>
+                            <div className='container-carregando'>
+                                <h2>Carregando...</h2>
+                            </div>
                         </>
-                    ) : null}
+                    ) : (
+                        <>
+                            <div className='info-album'>
+                                <img className='img' src={informacoesDoAlbum.image?.[2]?.['#text']} alt={informacoesDoAlbum.name} />
+                                <h2>{informacoesDoAlbum.name}</h2>
+                                <p className='p-artista'>{informacoesDoAlbum.artist}</p>
+
+                                <div className='container-tags'>
+                                    {tags?.length > 0 ? (
+                                        tags.map((tag, key) =>
+                                            <span className='tag' key={key}>{tag.name}</span>
+                                        )
+                                    ) : (
+                                        <p>Nenhuma tag encontrada!</p>
+                                    )
+                                    }
+                                </div>
+                            </div>
+
+                            <div className='musicas'>
+                                <ul className='lista-musicas'>
+                                    {musicas?.length > 0 ? (
+                                        musicas.map((musica, key) =>
+                                            <li className='item-musica' key={key}>
+                                                <p>{musica['@attr'].rank}.</p>
+                                                <p>{musica.name}</p>
+                                            </li>
+                                        )
+                                    ) : (
+                                        <p>Nenhuma música encontrada!</p>
+                                    )}
+                                </ul>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </>
